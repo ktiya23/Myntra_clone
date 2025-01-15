1bbe1a83a0411e1f2b4e99746c8877bafacdc218
ktiya23 <kashishagrahari23@gmail.com>
2025-01-15 23:06:00 +0530

    added files

--- frontend/src/components/TopPart/Brands.js
+++ frontend/src/components/TopPart/Brands.js
@@ -1,47 +1,136 @@
-import React from 'react'
+import React from "react";
 import { Container, Box } from "@mui/material";
-
 
 const Brands = () => {
   return (
-    <Container
-    sx={{
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      gap: 0,
-      margin: "0 auto",
-      mt: "100px",
-      p: 0,
-      overflow: "hidden",
-    }}
-  >
-    <Box sx={{ display: "flex", width: "100%" }}>
-      <img
-        src="https://assets.myntassets.com/f_webp,w_490,c_limit,fl_progressive,dpr_2.0/assets/images/2025/JANUARY/10/hOdRKVHJ_1144dd5f360a4bd38d79200ded0a1b6d.jpg"
-        alt="First"
-        style={{
-          width: "50%",
-          height: "auto",
-          margin: 0,
-          padding: 0,
-          display: "block",
+    <div>
+      <Container
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: 0,
+          margin: "0 auto",
+          mt: "0",
+          p: 0,
+          overflow: "hidden",
         }}
-      />
-      <img
-        src="https://assets.myntassets.com/f_webp,w_490,c_limit,fl_progressive,dpr_2.0/assets/images/2025/JANUARY/10/S0PybOMD_1edc8c14119b47ab9fe19db3f3d94ea3.jpg"
-        alt="Second"
-        style={{
-          width: "50%",
-          height: "auto",
-          margin: 0,
-          padding: 0,
-          display: "block",
+      >
+        <Box sx={{ display: "flex", width: "100%" }}>
+          <img
+            src="https://assets.myntassets.com/f_webp,w_326,c_limit,fl_progressive,dpr_2.0/assets/images/2025/JANUARY/12/kK059JIu_a31dc002a98d4900b4e79c624c7556e1.png"
+            alt="First"
+            style={{
+              width: "50%",
+              height: "auto",
+              margin: 0,
+              padding: 0,
+              display: "block",
+            }}
+          />
+          <img
+            src="https://assets.myntassets.com/f_webp,w_326,c_limit,fl_progressive,dpr_2.0/assets/images/2025/JANUARY/12/LjcGKuOd_1e645450e21844d9b998e64ccdeb9d1e.png"
+            alt="Second"
+            style={{
+              width: "50%",
+              height: "auto",
+              margin: 0,
+              padding: 0,
+              display: "block",
+            }}
+          />
+          <img
+            src="https://assets.myntassets.com/f_webp,w_326,c_limit,fl_progressive,dpr_2.0/assets/images/2025/JANUARY/12/8l7LcFED_feee6f7a4f5f44348a7dc66ab76666ba.png"
+            alt="Third"
+            style={{
+              width: "50%",
+              height: "auto",
+              margin: 0,
+              padding: 0,
+              display: "block",
+            }}
+          />
+        </Box>
+      </Container>
+      <Box
+        sx={{
+          width: "100%",
+          mt: 0, 
+          display: "flex",
+          justifyContent: "center",
         }}
-      />
-    </Box>
-  </Container>
-  )
-}
+      >
+        <img
+          src="https://assets.myntassets.com/f_webp,w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2025/JANUARY/1/UskhTCue_6dc2919021c148c28a2d1df36dfaf8af.jpg"
+          alt="Fourth"
+          style={{
+            width: "70%", 
+            height: "auto",
+            margin: 0,
+            padding: 0,
+            display: "block",
+          }}
+        />
+      </Box>
+      <Container
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: 0,
+            margin: "0 auto",
+            mt: "100px",
+            p: 0,
+            overflow: "hidden",
+          }}
+        >
+          <Box sx={{ display: "flex", width: "100%" }}>
+            <img
+              src="https://assets.myntassets.com/f_webp,w_490,c_limit,fl_progressive,dpr_2.0/assets/images/2025/JANUARY/10/4tkVHPqP_83b5945745f64d55ae610c181b9c7b67.jpg"
+              alt="First"
+              style={{
+                width: "50%",
+                height: "auto",
+                margin: 0,
+                padding: 0,
+                display: "block",
+              }}
+            />
+            <img
+              src="https://assets.myntassets.com/w_490,c_limit,fl_progressive,dpr_2.0/assets/images/2025/JANUARY/10/QmLuHLur_e2fa5970f9994be49654dc00711ed727.jpg"
+              alt="Second"
+              style={{
+                width: "50%",
+                height: "auto",
+                margin: 0,
+                padding: 0,
+                display: "block",
+              }}
+            />
+          </Box>
+        </Container>
+        <Box
+        sx={{
+          width: "100%",
+          mt: 0, 
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <img
+          src="https://assets.myntassets.com/f_webp,w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2025/JANUARY/11/3OiTbrsX_db00a4b8bb714efd956ec6339048e6f0.jpg"
+          alt="Fourth"
+          style={{
+            width: "70%", 
+            height: "auto",
+            margin: 0,
+            padding: 0,
+            display: "block",
+          }}
+        />
+      </Box>
+    </div>
+  );
+};
 
-export default Brands
+export default Brands;
--- frontend/src/components/TopPart/Top.js
+++ frontend/src/components/TopPart/Top.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Banner from "./Banner";
 import BankOffer from "./BankOffer";
+import Brands from "./Brands";
 
 const Top = () => {
   return (
     <div>
         <Banner/>
         <BankOffer/>
+        <Brands/>
     </div>
   );
 };

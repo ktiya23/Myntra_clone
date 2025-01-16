b347ba08e0abcf761b39195de427a8b7068a80e5
ktiya23 <kashishagrahari23@gmail.com>
2025-01-16 14:04:33 +0530

    added files

--- frontend/src/components/ShopByCategory/ShopByCategory.js
+++ frontend/src/components/ShopByCategory/ShopByCategory.js
@@ -14,15 +14,65 @@ const images=[
     {url:"https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2025/JANUARY/10/G0tDeU5X_47e68f1ab94a4289842265de4d1b6e83.jpg"},
     {url:"https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2025/JANUARY/10/LlQfGoh3_b449e189dd9b48c6b6ab6a59f36acef0.jpg"},
     {url:"https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2025/JANUARY/10/CEcuo6U4_3f4fc4a160c247cfb41e70485f281d54.jpg"},//
+    {url:"https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2025/JANUARY/10/Njb0ABov_45db62506a1b42798513e8b783c7a98b.jpg"},
+    {url:"https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2025/JANUARY/10/D9G4Kjrz_c1003161fae04485906a37715b01a4cf.jpg"},
+    {url:"https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2025/JANUARY/10/VfNti6y8_a2e5ae944ecc4736b18efb8a75b1e191.jpg"},
+    {url:"https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2025/JANUARY/10/uwqXL3Kq_0396ac2e5a1a4c12af1eec55c14917c8.jpg"},
+    {url:"https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2025/JANUARY/10/CdimVElj_ce97d46d5fe64428a08f51217b9f8b0b.jpg"},
+    {url:"https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2025/JANUARY/10/9Po69KXA_e28249e405fb478fb556ad7a92ca445e.jpg"},//
+    {url:"https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2025/JANUARY/10/f1hhXRXb_47fa218587514bf7902594e41d14e9f2.jpg"},
+    {url:"https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2025/JANUARY/10/hKMNGktP_ad33524adea34688b025f947573673cb.jpg"},
+    {url:"https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2025/JANUARY/10/Oa2sAEOX_a55bd8360faa48eeb39ae724b06b86ce.jpg"},
+    {url:"https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2025/JANUARY/10/5rDi1B23_c892152411964fd09fc7f6efd04c5fa0.jpg"},
+    {url:"https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2025/JANUARY/10/pfVXrZzB_dc0dcd4b3d774b10ad9df1073077bdcd.jpg"},
+    {url:"https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2025/JANUARY/10/Q3zid8FM_61065ffcdeac466895ae4ed15b708158.jpg"},//
+    {url:"https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2025/JANUARY/10/Om1hvOEr_5f550103a1764cc5ab3e09abdf1acaf5.jpg"},
+    {url:"https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2025/JANUARY/10/jPv6EcWn_7d8d6ab1e2ae48f9ad27305ed489e368.jpg"},
+    {url:"https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2025/JANUARY/10/Oyv2rmcn_4b883a02a10d43c1adb2a8b2f598324d.jpg"},
+    {url:"https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2025/JANUARY/10/3Vmva6Mn_d1d1003350df4a33be4218faf45bca1d.jpg"},
+    {url:"https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2025/JANUARY/10/NLt0jbrr_4842380d08cb4b2a9e1f735fc8af96a6.jpg"},
+    {url:"https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2025/JANUARY/10/Zthu96N4_af86499a08294db9acb0ed2248680fa5.jpg"},//
+    {url:"https://assets.myntassets.com/w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2025/JANUARY/10/tXjn3xva_ffd20b0d874a4344829952735ed174a5.jpg"},
+    {url:"https://assets.myntassets.com/w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2025/JANUARY/10/CQljGEhb_651bd02ed5ba4444af2b4e37ab93ec0b.jpg"},
+    {url:"https://assets.myntassets.com/w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2025/JANUARY/10/zGx6jkkw_e735b26722574430846f697fa8c7b717.jpg"},
 
 ]
 
 const ShopByCategory = () => {
   return (
-    <Container>
-        <img src='https://assets.myntassets.com/f_webp,w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2025/JANUARY/11/fn1avydK_9837e0b576384390acc9607b89ed5bfc.jpg' alt='ShopByCategory' style={{display:"flex", justifyContent:"centre" , alignItems:"center" , height:"auto", margin:"0 auto", width:"100%" }}/>
-        
-    </Container>
+    <Container maxWidth="xl">
+        <img src='https://assets.myntassets.com/f_webp,w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2025/JANUARY/11/fn1avydK_9837e0b576384390acc9607b89ed5bfc.jpg' alt='ShopByCategory' style={{display:"block", height:"auto", margin:"0 auto", width:"90%" }}/>
+
+        <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap", 
+          justifyContent: "center",
+          margin:0,
+          padding:0,
+        }}
+      >
+        {images.map((image, index) => (
+          <div
+            key={index}
+            style={{
+              flex: "0 0 calc(16.66% - 16px)", 
+            }}
+          >
+            <img
+              src={image.url}
+              alt={`Category ${index + 1}`}
+              style={{
+                width: "100%",
+                height: "auto",
+                margin:0,
+                padding:0
+              }}
+            />
+          </div>
+        ))}
+      </div>
+     </Container>
   )
 }
 

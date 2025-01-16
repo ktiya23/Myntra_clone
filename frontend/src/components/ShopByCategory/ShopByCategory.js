d21a5425e491ebdd70b19e387275b08cf604df47
ktiya23 <kashishagrahari23@gmail.com>
2025-01-16 13:20:03 +0530

    added files

--- frontend/src/components/ShopByCategory/ShopByCategory.js
+++ frontend/src/components/ShopByCategory/ShopByCategory.js
@@ -1,10 +1,27 @@
-import { Container, ImageList, ImageListItem, Typography } from '@mui/material'
+import { Container, Grid2, } from '@mui/material'
 import React from 'react'
+
+const images=[
+    {url:"https://assets.myntassets.com/f_webp,w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2025/JANUARY/10/hpEcrOrm_6a9dac6181ed4930aa2a7e12c5150175.jpg"},
+    {url:"https://assets.myntassets.com/f_webp,w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2025/JANUARY/10/50e3kk1G_e49c76b1d4634c7681c2662fb2fd3095.jpg"},
+    {url:"https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2025/JANUARY/10/gx4ejH2p_e11304b9b8fe400baa493609fe88e365.jpg"},
+    {url:"https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2025/JANUARY/10/5GPoLjgx_802886f8b9c74273803313b1103a8e16.jpg"},
+    {url:"https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2025/JANUARY/10/ZBB1zqm6_120b40ac77a04d3db7503b4c6a13dd91.jpg"},
+    {url:"https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2025/JANUARY/10/yg29mbde_9659810f5c6e4b629c60d86afd9fec8a.jpg"},//
+    {url:"https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2025/JANUARY/10/bIQP9wbr_67b3a95df792456b98bc1ba1237bb779.jpg"},
+    {url:"https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2025/JANUARY/10/2nVCvWU8_486c820b2db44cf8bdd4678c988cf821.jpg"},
+    {url:"https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2025/JANUARY/10/iDYvU5BX_4a545298ef67430b8f42bc0566a925d1.jpg"},
+    {url:"https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2025/JANUARY/10/G0tDeU5X_47e68f1ab94a4289842265de4d1b6e83.jpg"},
+    {url:"https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2025/JANUARY/10/LlQfGoh3_b449e189dd9b48c6b6ab6a59f36acef0.jpg"},
+    {url:"https://assets.myntassets.com/w_163,c_limit,fl_progressive,dpr_2.0/assets/images/2025/JANUARY/10/CEcuo6U4_3f4fc4a160c247cfb41e70485f281d54.jpg"},//
+
+]
 
 const ShopByCategory = () => {
   return (
     <Container>
         <img src='https://assets.myntassets.com/f_webp,w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2025/JANUARY/11/fn1avydK_9837e0b576384390acc9607b89ed5bfc.jpg' alt='ShopByCategory' style={{display:"flex", justifyContent:"centre" , alignItems:"center" , height:"auto", margin:"0 auto", width:"100%" }}/>
+        
     </Container>
   )
 }

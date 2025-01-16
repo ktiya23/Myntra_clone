8a0cea5d8608a0db6fc7de55ed682a241d4eca69
ktiya23 <kashishagrahari23@gmail.com>
2025-01-16 14:39:28 +0530

    added files

--- frontend/src/components/FooterPart/Footer.js
+++ frontend/src/components/FooterPart/Footer.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import { Container } from '@mui/material';
+import React from 'react';
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Container
+      sx={{
+        backgroundColor: "rgba(164, 165, 167, 0.2)",        
+        width: "100vw", // Full screen width
+        height: "100vh", // Full screen height
+      }}
+    >
+      hgbjcxmzjk
+    </Container>
+  );
+};
 
-export default Footer
+export default Footer;

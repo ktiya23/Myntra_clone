38847b203b457ab36690fcfec2c2f90a11fa4279
ktiya23 <kashishagrahari23@gmail.com>
2025-01-17 21:39:46 +0530

    added files

---
+++ backend/Controllers/AuthController.js
@@ -0,0 +1,9 @@
+const signup = async (req,res)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+export default signup;
--- backend/Routes/AuthRouter.js
+++ backend/Routes/AuthRouter.js
@@ -1,12 +1,12 @@
 import express from "express";
+import signupValidation from ("../Middlewares/AuthValidation")
+import signup from "../Controllers/AuthController";
 const router = express.Router();
 
 router.post("/login", (req, res) => {
     res.send("Login Success");
 });
 
-router.post("/signup", (req, res) => {
-    res.send("Signup Success");
-});
+router.post("/signup", signupValidation,signup );
 
 export default router;  

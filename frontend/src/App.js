adc5292a4288e34813503131d204ddcfaa6010cb
ktiya23 <kashishagrahari23@gmail.com>
2025-01-19 22:00:34 +0530

    added files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Footer from './components/FooterPart/Footer';
 import Header from './components/HeaderPart/Header';
 import ImageSlider from './components/ImageSliderPart/ImageSlider';
+import CartPage from './components/Pages/CartPage';
 import LoginPage from './components/Pages/LoginPage';
 import SignupPage from './components/Pages/SignupPage';
 import WishListPage from './components/Pages/WishListPage';
@@ -27,6 +28,7 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage/>} />
         <Route path='/wishlist' element={<WishListPage/>} />
+        <Route path='/cart' element={<CartPage />} />
       </Routes>
       <Footer/>
     </div>

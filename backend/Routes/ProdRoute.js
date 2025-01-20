e2d806d00fadad320d435f15cd10e89cd4d78498
ktiya23 <kashishagrahari23@gmail.com>
2025-01-20 15:14:30 +0530

    added files

--- backend/Routes/ProdRoute.js
+++ backend/Routes/ProdRoute.js
@@ -1,4 +1,7 @@
 import express from "express";
-const router = express.Router()
-const {getProductDetails} from "../Controllers/ProdController.js"
-router.get("/products/:category",getProductDetails)
+const router = express.Router();
+import { getProductDetails } from "../Controllers/ProdController.js";
+
+router.get("/products/:category", getProductDetails);
+
+export default router;
--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,7 @@ app.use(cors({
     credentials: true
 }));
 app.use("/auth", AuthRouter);
+app.use("/api" , Pro)
 
 Connection();
 
